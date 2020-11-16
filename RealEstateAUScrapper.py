509e550bb496f49480411b8cb12614444c8bafe3
Andrew Chen <achen4824@gmail.com>
2020-11-16 23:52:56 +1030

    Selenium working

--- RealEstateAUScrapper.py
+++ RealEstateAUScrapper.py
@@ -2,14 +2,22 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
 
-driver = webdriver.Firefox()
-driver.get('https://www.realestate.com.au/sold/in-wayville,+sa+5034%3b/list-1')
+profile = webdriver.FirefoxProfile()
+profile.set_preference("general.useragent.override", "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:82.0) Gecko/20100101 Firefox/82.0")
+driver = webdriver.Firefox(profile)
+driver.get('https://www.realestate.com.au/sold')
 
-# wait for "video" to be present
-video = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, "page-wrapper")))
+driver.get_cookies()
+
+search = driver.find_element(By.XPATH,'//*[@id="where"]')
+search.send_keys("5062")
+
+searchbutton = driver.find_element(By.XPATH,'/html/body/div[1]/div[1]/div[1]/form/div/div[1]/div/div/button')
+searchbutton.click()
 
 # get the src value
-print(video.get_attribute("src"))
+# print(video.get_attribute("src"))
 
-driver.close()
+# driver.close()
